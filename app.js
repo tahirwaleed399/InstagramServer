c8cb677f1db36d64db3709466d7df16f0e67ea57
Muhammad Waleed Tahir <73441359+tahirwaleed399@users.noreply.github.com>
2023-02-11 22:38:37 +0500

    added puclic

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const userRouter =  require('./Routes/User');
 const { jsonResponce } = require('./Utils/responce.js');
 const NewErrorHandler = require('./Utils/NewErrorHandler');
 const { ErrorController } = require('./Controllers/ErrorController.js');
+const path = require('path');
 app.use(express.json());
 app.use(cors({
     origin: ['http://localhost:3000'],
     
     credentials: true,
 }))
-
+app.use(express.static(path.resolve("./public")));
 app.use(userRouter);
 app.all('*', (req, res , next)=>next(new NewErrorHandler('Route Not Found' , 404)))
 app.use(ErrorController)
